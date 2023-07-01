a632678c6fbc52e7a9cc9b85901bd56acb53f4a1
Jalen Liwan <liwanjalenzki@gmail.com>
2023-07-01 16:49:57 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// Create two variables to store choices, one for user and another for computer
+
+// Ask user input (rock, paper or scissors)
+
+// Compare the two variables to determine the winner
+
+// Print the winner in the console
