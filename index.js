2c3f6289ae9f72ae15275476cca2c9b466294bc7
Jalen Liwan <liwanjalenzki@gmail.com>
2023-07-01 17:20:27 +0800

    working console game

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 // Create two variables to store choices, one for user and another for computer
-
+const choices = ["rock", "paper", "scissor"];
+const comp_input = Math.floor(Math.random() * 3);
+let user_input = 0;
 // Ask user input (rock, paper or scissors)
-
-// Compare the two variables to determine the winner
-
-// Print the winner in the console
+user_input = parseInt(prompt("Pick: (1)Rock  (2)Paper  (3)Scissors")) - 1;
+//Print their choices
+console.log(`Computer: ${choices[comp_input]}\t User: ${choices[user_input]}`);
+// Compare the two variables to determine the winner & Print the winner in the console
+if (comp_input === 1 && user_input === 2) {
+  console.log("User won!");
+} else if (comp_input === 1 && user_input === 3) {
+  console.log("Computer won!");
+} else if (comp_input === 2 && user_input === 1) {
+  console.log("Computer won!");
+} else if (comp_input === 2 && user_input === 3) {
+  console.log("User won!");
+} else if (comp_input === 3 && user_input === 1) {
+  console.log("User won!");
+} else if (comp_input === 3 && user_input === 2) {
+  console.log("Computer won!");
+} else {
+  console.log("Draw!");
+}
