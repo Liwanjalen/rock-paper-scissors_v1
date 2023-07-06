eb3618ee21df40107a78c5c837a21886a52ce96a
Jalen Liwan <liwanjalenzki@gmail.com>
2023-07-06 20:52:53 +0800

    add 3 buttons with click event listener

--- index.js
+++ index.js
@@ -3,14 +3,7 @@ const choices = ["rock", "paper", "scissor"];
 
 function getComputerChoice() {
   const comp_input = Math.floor(Math.random() * 3);
-  return comp_input;
-}
-
-// Ask user input (rock, paper or scissors)
-function getUserChoice() {
-  const user_input =
-    parseInt(prompt("Pick: (1)Rock  (2)Paper  (3)Scissors")) - 1;
-  return user_input;
+  return choices[comp_input];
 }
 
 // Compare the two variables to determine the winner
@@ -18,35 +11,42 @@ function play(computerSelection, playerSelection) {
   let winner = "";
   console.log(computerSelection + " " + playerSelection);
 
-  console.log(
-    `Computer: ${choices[computerSelection]}\t User: ${choices[playerSelection]}`
-  );
-
-  if (computerSelection === 0 && playerSelection === 1) {
+  if (computerSelection === "rock" && playerSelection === "paper") {
     winner = "User won!";
-  } else if (computerSelection === 0 && playerSelection === 2) {
+  } else if (computerSelection === "rock" && playerSelection === "scissor") {
     winner = "Computer won!";
-  } else if (computerSelection === 1 && playerSelection === 0) {
+  } else if (computerSelection === "paper" && playerSelection === "rock") {
     winner = "Computer won!";
-  } else if (computerSelection === 1 && playerSelection === 2) {
+  } else if (computerSelection === "paper" && playerSelection === "scissor") {
     winner = "User won!";
-  } else if (computerSelection === 2 && playerSelection === 0) {
+  } else if (computerSelection === "scissor" && playerSelection === "rock") {
     winner = "User won!";
-  } else if (computerSelection === 2 && playerSelection === 1) {
+  } else if (computerSelection === "scissor" && playerSelection === "paper") {
     winner = "Computer won!";
   } else {
     winner = "Draw!";
   }
 
+  console.log(winner);
+
   return winner;
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    const player = getUserChoice();
-    const computer = getComputerChoice();
-    console.log(play(computer, player));
-  }
+// const buttons = document.querySelectorAll(".playerChoice");
+
+// buttons.forEach((button) => {
+//   const player = button.id;
+//   const computer = getComputerChoice();
+//   button.addEventListener("click", function () {
+//     alert("running");
+//   });
+//   console.log(button);
+// });
+function startGame(e) {
+  const player = e.target.id;
+  if (!player) return;
+  const computer = getComputerChoice();
+  play(computer, player);
 }
 
-game();
+window.addEventListener("click", startGame);
